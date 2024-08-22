3ca31dc5f60655f82a6a8848f6eb25a6db46a36a
Ethan Nunez <ern1274@rit.edu>
2024-08-22 16:52:09 -0500

    Modified link and added extra parameters for set up of csv downloads

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # NREL_DataMining
 A Practice/Showcase project of NREL Dataset mining
+
+Premise: 
+The NSRDB is a database collection of meteorological data containing measurements calculating solar radiation which can be converted to energy. NSRDB is a publicly available database which charges nothing for their data.
+
+Goal:
+Set up options for Data Mining Analysis techniques to be done on data from a specific region if available. 
+
+Goal includes:
+- Set up ETL Process to make data readable to the machine
+- Add query modification options: Location (Latitude and Longitude or Country, City, Address and Zip), if given only a country and zip/city and state/, find the box around that region and make a region query
+- TBD
--- main.c
+++ main.c
@@ -4,16 +4,34 @@
 #include <curl/curl.h>
 #include "util.h"
 #include "key.h"
+#include <regex.h>    
 
 extern char *key;
+extern char *email;
+
+// Considering REGEX for data tokenization
+void printData(char *data) {
+    char *token;
+	if(data == NULL) {
+        printf("Null data");
+        return ;
+    }
+	while ((token = strsep(&data, ",")) != NULL) {
+        printf("%s\n", token);
+    }
+}
+
 int main(int argc, char *argv[]) {
     if( argc != 2 ) {
         printf("usage: try './curl [url]' to make a get request.\n");
         return 1;
     }
     
-    char* link = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=";
-    char* link_key = malloc((strlen(link) + strlen(key)) * sizeof(char));    strcat(link_key, link);
+    char* link = "https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-2-2-download.json?&limit=1&location_ids=681462&years=2020,2021,2022";
+    // Add modification options such as yeats, location_ids, limit and such
+    char* link_key = malloc((strlen(link) + strlen(key) + strlen(email)) * sizeof(char));    
+    strcat(link_key, link); 
+    strcat(link_key, email);
     strcat(link_key, key);
 
     struct MemoryStruct chunk;
@@ -24,17 +42,17 @@ int main(int argc, char *argv[]) {
     if(curl_handle) {
         printf("\nLink: %s", link_key);
         curl_easy_setopt(curl_handle, CURLOPT_URL, link_key);
-        //curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1L);
-        //curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
-        //curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
 
         CURLcode res = curl_easy_perform(curl_handle);
 
         if(res != CURLE_OK) {
             fprintf(stderr, "error: %s\n", curl_easy_strerror(res));
         } else {
-            printf("Size: %lu\n", (unsigned long)chunk.size);
-            printf("Data: %s\n", chunk.memory);
+            printf("\nSize: %lu\n", (unsigned long)chunk.size);
+            printData(chunk.memory);
+            //printf("\nData: %s\n", chunk.memory);
         }
         curl_easy_cleanup(curl_handle);
         free(chunk.memory);
