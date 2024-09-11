fe53aa42ae212492e9a217bc35896a26588a4625
Ethan Nunez <ern1274@rit.edu>
2024-09-11 11:14:28 -0500

    overhaul old data download system to pandas request and export to dataframe method

--- src/NREL_Request.py
+++ src/NREL_Request.py
@@ -7,99 +7,72 @@ Language: Python 3.8
 Author: Ethan Ray Nunez     ern1274@rit.edu
 """
 
-from NREL_DataMining import config
+from NREL_DataMining.src import config
 import os
-import requests
-import zipfile
 import ctypes
+import pandas as pd
 
-url = "https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-2-2-download.json"
-cwd = os.getcwd()
-zipPath =  cwd +'/SolarData.zip'
-dirPath = cwd + '/Data'
+url = "https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-download.csv?"
 api_key = config.api_key
 email = config.email
-so_file = cwd + '/analyze.so'
+so_file = os.getcwd() + '/analyze.so'
 cMethods = ctypes.CDLL(so_file)
 
-"""
-downloadData: String( A )
-    where A is the download link for a zipfile. 
-    Once zip file is downloaded, files within are unpacked and stored 
-"""
-def downloadData(downloadUrl):
-    fileName = "SolarData.zip"
-    response = requests.get(downloadUrl)
-    with open(fileName, mode="wb") as file:
-        file.write(response.content)
 
-    with zipfile.ZipFile(zipPath, 'r') as zip:
-        for member in zip.infolist():
-            arr = member.filename.split("/")
-            zip.extract(member, dirPath)
-            os.rename(dirPath + "/" + member.filename, dirPath + "/" + arr[1])
-    os.remove(zipPath)
-"""
-extractAndClean: () -> List(String), List(List(float))
-    Must be called after downloadData function, iterates lines in each FILE within the Data folder
-    and extracts and stores each value from their respective file into a data structure.
-    
-    Returns the list of headers and an array of arrays of values for each header
-"""
-def extractAndClean():
-    #metadata = []
-    #metadataValues = []
-    headers = []
-    values = []
-    for filename in os.scandir(dirPath):
-        f = os.path.join(dirPath, filename)
-        if os.path.isfile(f):
-            print(f)
-            with open(f, mode="r") as file:
-                lines = file.readlines()
-                for h in range(len(lines)):
-                    line = lines[h]
-                    arr = line.split(',')
-                    if h < 3: # For headers
-                        #if len(metadata) == 0 and h == 0:
-                        #    metadata = arr
-                        #elif len(metadataValues) == 0 and h == 1:
-                        #    metadataValues = arr
-                        if len(headers) == 0 and h == 2:
-                            headers = headers + arr
-                    else:
-                        for i in range(len(headers)):
-                            if len(values) == i:
-                                values.append([])
-                            values[i].append(float(arr[i]))
-    return headers, values
+def exportToDF():
+    lat, lon, year = 32.9741,-106.2, 2020
+    # Set the attributes to extract (e.g., dhi, ghi, etc.), separated by commas.
+    attributes = 'ghi,dhi,dni,wind_speed,air_temperature,solar_zenith_angle'
+    # Choose year of data
+    year = '2020'
+    # Set leap year to true or false. True will return leap day data if present, false will not.
+    leap_year = 'false'
+    # Set time interval in minutes, i.e., '30' is half hour intervals. Valid intervals are 30 & 60.
+    interval = '30'
+    # Specify Coordinated Universal Time (UTC), 'true' will use UTC, 'false' will use the local time zone of the data.
+    # NOTE: In order to use the NSRDB data in SAM, you must specify UTC as 'false'. SAM requires the data to be in the
+    # local time zone.
+    utc = 'false'
+    # Your full name, use '+' instead of spaces.
+    your_name = 'Ethan+Nunez'
+    # Your reason for using the NSRDB.
+    reason_for_use = 'Academic'
+    # Your affiliation
+    your_affiliation = 'Rochester+Institute+of+Technology'
+    email = 'ethanray2002@gmail.com'
+
+    link = url+'&api_key={api}&wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&affiliation={affiliation}&reason={reason}&attributes={attr}&email={email}'.format(
+        year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, affiliation=your_affiliation, reason=reason_for_use, api=api_key, email=email, attr=attributes)
+    # Return just the first 2 lines to get metadata:
+    #print(link)
+    info = pd.read_csv(link, nrows=1)
+    # See metadata for specified properties, e.g., timezone and elevation
+    timezone, elevation = info['Local Time Zone'], info['Elevation']
+    #print(info)
+    df = pd.read_csv(
+        url+'api_key={api}&wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&affiliation={affiliation}&reason={reason}&attributes={attr}&email={email}'.format(
+            year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, affiliation=your_affiliation, reason=reason_for_use, api=api_key, email=email,
+            attr=attributes), skiprows=2)
+    print(df.columns.values)
+    #print(type(df['Year'].values))
+    return df
+
 """
 main: 
     Forms a data request based on pre-established values and calls functions 
     to download data, extract, clean and conduct data analysis on NREL Solar Data
 """
 def main():
-    payload = "&api_key="+api_key + "&years=2021&leap_day=false&interval=60&utc=false&reason=Academic&wkt=MULTIPOINT(-106.22%2032.9741%2C-106.18%2032.9741%2C-106.1%2032.9741)"
-    payload += "&email=" + email
-    headers = {
-        'content-type': "application/x-www-form-urlencoded",
-        'cache-control': "no-cache",
-    }
-    response = requests.get(url, params=payload, headers=headers)
-    #response = requests.get(url)
-    print(response.text)
-    downloadUrl = response.json()['outputs']['downloadUrl']
-    print(downloadUrl)
-    downloadData(downloadUrl)
-
-    headers, values = extractAndClean()
-    length = len(values[0])
+    df = exportToDF()
+    values = df['Year'].values
+    length = len(values)
     c_array_type = ctypes.c_double *length
-    arr = c_array_type(*values[0])
+    arr = c_array_type(*values)
 
     cMethods.centralTendency.argtypes = [ctypes.Array, ctypes.c_int]
 
 
     cMethods.centralTendency(arr, length)
+
 
 main()
